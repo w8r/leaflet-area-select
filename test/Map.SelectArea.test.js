ed13d6d54890f9ea7dc48133196783dc40b64f39
charlieforward9 <charlesrichardsonusa@gmail.com>
2024-06-03 18:03:58 -0400

    fix Expose start/end coordinates #42

--- test/Map.SelectArea.test.js
+++ test/Map.SelectArea.test.js
@@ -1,16 +1,16 @@
-import tape  from 'tape';
-import L from 'leaflet';
-require('../');
+import tape from "tape";
+import L from "leaflet";
+require("../");
 
-function triggerEvent (element, type, options = {}) {
+function triggerEvent(element, type, options = {}) {
   var evt = new MouseEvent(type, {
-    'view': window,
-    'bubbles': true,
-    'cancelable': true,
-    'clientX': options.clientX,
-    'clientY': options.clientY,
-    'which': options.which,
-    'ctrlKey': true
+    view: window,
+    bubbles: true,
+    cancelable: true,
+    clientX: options.clientX,
+    clientY: options.clientY,
+    which: options.which,
+    ctrlKey: true,
   });
   //evt.initEvent(type, false, true);
 
@@ -22,34 +22,35 @@ function triggerEvent (element, type, options = {}) {
   return evt;
 }
 
-tape('L.Map.SelectArea', (t) => {
-
+tape("L.Map.SelectArea", (t) => {
   function createMap() {
-    let container = L.DomUtil.create('div', 'map', document.body);
+    let container = L.DomUtil.create("div", "map", document.body);
     let map = L.map(container).setView([22.42658, 114.1452], 11);
     return map;
   }
 
-  t.test('exposed', (t) => {
-    t.ok(L.Map.SelectArea, 'exposed');
-    t.equal(L.Map.prototype.options.selectArea, false, 'in map options');
+  t.test("exposed", (t) => {
+    t.ok(L.Map.SelectArea, "exposed");
+    t.equal(L.Map.prototype.options.selectArea, false, "in map options");
     t.end();
   });
 
-  t.test('interaction', (t) => {
-    t.plan(4);
+  t.test("interaction", (t) => {
+    t.plan(6);
 
     let map = createMap();
 
     map.on({
-      'areaselecttoggled': (e) => {
-        t.pass('toggled');
+      areaselecttoggled: (e) => {
+        t.pass("toggled");
       },
-      'areaselected': (e) => {
-        t.ok(e.bounds, 'bounds received');
-      }
+      areaselected: (e) => {
+        t.ok(e.bounds, "bounds received");
+        t.ok(e.start, "start point received");
+        t.ok(e.end, "end point received");
+      },
     });
-    t.ok(map.selectArea, 'handler instance');
+    t.ok(map.selectArea, "handler instance");
     map.selectArea.enable();
 
     // otherwise tests are broken,
@@ -57,25 +58,25 @@ tape('L.Map.SelectArea', (t) => {
     // cannot work with fake events
     map.dragging.disable();
 
-    triggerEvent(map.selectArea._container, 'mousedown', {
+    triggerEvent(map.selectArea._container, "mousedown", {
       clientX: 100,
-      clientY:100,
+      clientY: 100,
       ctrlKey: true,
       // target: map._container,
-      which: 1 // button
+      which: 1, // button
     });
 
     setTimeout(() => {
-      t.ok(map.selectArea._startLayerPoint, 'started selection');
+      t.ok(map.selectArea._startLayerPoint, "started selection");
 
-      triggerEvent(document, 'mousemove', {
+      triggerEvent(document, "mousemove", {
         clientX: 200,
-        clientY: 200
+        clientY: 200,
       });
 
-      triggerEvent(document, 'mouseup', {
+      triggerEvent(document, "mouseup", {
         clientX: 201,
-        clientY: 201
+        clientY: 201,
       });
     }, 100);
   });
